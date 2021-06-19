8146768c491d6be11f4475d855b0fc92ac617963
Cheng-Yu Pai <mail4charliepai@gmail.com>
2021-06-19 03:48:19 -0700

    Add flux animation to hero sprite

--- core/src/main/java/com/pipai/dragontiles/artemis/systems/animation/PlayerFluxDamageAnimation.kt
+++ core/src/main/java/com/pipai/dragontiles/artemis/systems/animation/PlayerFluxDamageAnimation.kt
@@ -1,12 +1,22 @@
 package com.pipai.dragontiles.artemis.systems.animation
 
+import com.artemis.ComponentMapper
+import com.pipai.dragontiles.artemis.components.HeroComponent
 import com.pipai.dragontiles.artemis.systems.ui.TopRowUiSystem
+import com.pipai.dragontiles.utils.allOf
+import com.pipai.dragontiles.utils.fetch
 
 class PlayerFluxDamageAnimation(private val damage: Int) : Animation() {
+    private lateinit var mHero: ComponentMapper<HeroComponent>
 
     private lateinit var sTop: TopRowUiSystem
 
     override fun startAnimation() {
+        val cHero = mHero.get(world.fetch(allOf(HeroComponent::class)).first())
+        cHero.flux += damage
+        if (cHero.flux > cHero.fluxMax) {
+            cHero.flux = cHero.fluxMax
+        }
         sTop.setFluxRelative(damage)
         endAnimation()
     }
--- core/src/main/java/com/pipai/dragontiles/artemis/systems/animation/PlayerLoseFluxAnimation.kt
+++ core/src/main/java/com/pipai/dragontiles/artemis/systems/animation/PlayerLoseFluxAnimation.kt
@@ -1,12 +1,22 @@
 package com.pipai.dragontiles.artemis.systems.animation
 
+import com.artemis.ComponentMapper
+import com.pipai.dragontiles.artemis.components.HeroComponent
 import com.pipai.dragontiles.artemis.systems.ui.TopRowUiSystem
+import com.pipai.dragontiles.utils.allOf
+import com.pipai.dragontiles.utils.fetch
 
 class PlayerLoseFluxAnimation(private val amount: Int) : Animation() {
+    private lateinit var mHero: ComponentMapper<HeroComponent>
 
     private lateinit var sTop: TopRowUiSystem
 
     override fun startAnimation() {
+        val cHero = mHero.get(world.fetch(allOf(HeroComponent::class)).first())
+        cHero.flux -= amount
+        if (cHero.flux < 0) {
+            cHero.flux = 0
+        }
         sTop.setFluxRelative(-amount)
         endAnimation()
     }
