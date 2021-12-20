75d232064e5b26c3a4640bb872bb3a8b1247bd12
Cheng-Yu Pai <mail4charliepai@gmail.com>
2021-12-20 03:07:16 -0800

    Add batch animation logging

--- core/src/main/java/com/pipai/dragontiles/artemis/systems/animation/BatchAnimation.kt
+++ core/src/main/java/com/pipai/dragontiles/artemis/systems/animation/BatchAnimation.kt
@@ -1,11 +1,16 @@
 package com.pipai.dragontiles.artemis.systems.animation
 
+import com.pipai.dragontiles.utils.getLogger
+
 class BatchAnimation : Animation(), AnimationObserver {
+
+    private val logger = getLogger()
 
     private val animations: MutableList<Animation> = mutableListOf()
 
     override fun startAnimation() {
         animations.forEach {
+            logger.info("Start batch animation: $it")
             it.init(this.world, this.game)
             it.initObserver(this)
             it.startAnimation()
