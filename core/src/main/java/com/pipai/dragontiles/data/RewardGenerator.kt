cc3fec933c96de88d04ec018be101ed3dbc108a7
Cheng-Yu Pai <mail4charliepai@gmail.com>
2021-12-26 02:20:12 -0800

    proper reward distro

--- core/src/main/java/com/pipai/dragontiles/data/RewardGenerator.kt
+++ core/src/main/java/com/pipai/dragontiles/data/RewardGenerator.kt
@@ -1,11 +1,15 @@
 package com.pipai.dragontiles.data
 
 import com.pipai.dragontiles.combat.CombatRewardConfig
+import com.pipai.dragontiles.combat.SpellRewardType
+import com.pipai.dragontiles.hero.HeroClass
 import com.pipai.dragontiles.potions.Potion
 import com.pipai.dragontiles.relics.RelicInstance
 import com.pipai.dragontiles.spells.Rarity
+import com.pipai.dragontiles.spells.Spell
 import com.pipai.dragontiles.spells.SpellInstance
 import com.pipai.dragontiles.utils.choose
+import com.pipai.dragontiles.utils.withoutAll
 import kotlin.random.Random
 
 class RewardGenerator {
@@ -14,7 +18,7 @@ class RewardGenerator {
         val actualRewards = mutableListOf<Reward>()
         val heroClass = gameData.getHeroClass(runData.hero.heroClassId)
         val rng = runData.seed.rewardRng()
-        actualRewards.add(Reward.SpellDraftReward(heroClass.getRandomClassSpells(runData.seed, 3).map { it.toInstance() }))
+        actualRewards.add(Reward.SpellDraftReward(generateCardRewards(heroClass, rewardConfig.spellRewardType, rng, 3)))
         actualRewards.add(Reward.GoldReward(rewardConfig.gold))
         if (rng.nextFloat() < rewardConfig.potionChance) {
             actualRewards.add(Reward.PotionReward(choosePotion(gameData, rng).id))
@@ -30,6 +34,40 @@ class RewardGenerator {
         }
         runData.combatRewards.clear()
         runData.combatRewards.addAll(actualRewards)
+    }
+
+    fun generateCardRewards(heroClass: HeroClass, spellRewardType: SpellRewardType, rng: Random, amount: Int): List<SpellInstance> {
+        val rewards: MutableList<SpellInstance> = mutableListOf()
+        repeat(amount) {
+            var rare = 0f
+            var uncommon = 0f
+            when (spellRewardType) {
+                SpellRewardType.STANDARD -> {
+                    rare = 0.05f
+                    uncommon = 0.4f
+                }
+                SpellRewardType.ELITE -> {
+                    rare = 0.1f
+                    uncommon = 0.5f
+                }
+                SpellRewardType.BOSS -> {
+                    rare = 1f
+                }
+            }
+            val n = rng.nextFloat()
+            val rarityRoll = when {
+                n < rare -> Rarity.RARE
+                n >= rare && n < uncommon -> Rarity.UNCOMMON
+                else -> Rarity.COMMON
+            }
+            val chosen = heroClass.spells
+                .filter { it.rarity == rarityRoll }
+                .map { it.toInstance() }
+                .withoutAll(rewards)
+                .choose(rng)
+            rewards.add(chosen)
+        }
+        return rewards
     }
 
     fun choosePotion(gameData: GameData, rng: Random): Potion {
