0f718530f65afcc8d71d780e14548f5b5793c384
Cheng-Yu Pai <mail4charliepai@gmail.com>
2022-04-25 20:55:33 -0700

    change cold hands to attack

--- core/src/main/java/com/pipai/dragontiles/spells/elementalist/ColdHands.kt
+++ core/src/main/java/com/pipai/dragontiles/spells/elementalist/ColdHands.kt
@@ -3,6 +3,7 @@ package com.pipai.dragontiles.spells.elementalist
 import com.pipai.dragontiles.combat.CombatApi
 import com.pipai.dragontiles.combat.RandomTileStatusInflictStrategy
 import com.pipai.dragontiles.combat.TileStatusInflictStrategy
+import com.pipai.dragontiles.data.Element
 import com.pipai.dragontiles.data.TileStatus
 import com.pipai.dragontiles.spells.*
 import com.pipai.dragontiles.status.BreakStatus
@@ -12,15 +13,18 @@ import com.pipai.dragontiles.utils.getStackableCopy
 class ColdHands : StandardSpell() {
     override val id: String = "base:spells:ColdHands"
     override val requirement: ComponentRequirement = Identical(2, SuitGroup.ICE)
-    override val type: SpellType = SpellType.EFFECT
+    override val type: SpellType = SpellType.ATTACK
     override val targetType: TargetType = TargetType.SINGLE_ENEMY
     override val rarity: Rarity = Rarity.COMMON
     override val aspects: MutableList<SpellAspect> = mutableListOf(
+        AttackDamageAspect(2),
         StackableAspect(Weak(3, false), 1),
-        FluxGainAspect(1)
+        FluxGainAspect(2),
     )
 
     override suspend fun onCast(params: CastParams, api: CombatApi) {
+        val target = api.getEnemy(params.targets.first())
+        api.attack(target, Element.ICE, baseDamage(), flags())
         api.addStatusToEnemy(api.getEnemy(params.targets.first()), aspects.getStackableCopy(Weak::class))
         api.inflictTileStatusOnHand(
             RandomTileStatusInflictStrategy(
