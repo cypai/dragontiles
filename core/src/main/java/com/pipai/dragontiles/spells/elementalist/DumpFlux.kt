c46a2345555f4e66f25d45c2ad893c979923de88
Cheng-Yu Pai <mail4charliepai@gmail.com>
2022-04-28 00:48:00 -0700

    dump flux

---
+++ core/src/main/java/com/pipai/dragontiles/spells/elementalist/DumpFlux.kt
@@ -0,0 +1,19 @@
+package com.pipai.dragontiles.spells.elementalist
+
+import com.pipai.dragontiles.combat.CombatApi
+import com.pipai.dragontiles.spells.*
+
+class DumpFlux : StandardSpell() {
+    override val id: String = "base:spells:DumpFlux"
+    override val requirement: ComponentRequirement = Identical(4)
+    override val type: SpellType = SpellType.EFFECT
+    override val targetType: TargetType = TargetType.NONE
+    override val rarity: Rarity = Rarity.RARE
+    override val aspects: MutableList<SpellAspect> = mutableListOf(
+        FluxLossAspect(35),
+        ExhaustAspect(),
+    )
+
+    override suspend fun onCast(params: CastParams, api: CombatApi) {
+    }
+}
