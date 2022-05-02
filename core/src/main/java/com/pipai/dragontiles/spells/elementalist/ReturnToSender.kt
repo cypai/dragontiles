7c448bb39a956c39c8c5891dd9bde1131746cac1
Cheng-Yu Pai <mail4charliepai@gmail.com>
2022-05-02 02:49:58 -0700

    buff return to sender

--- core/src/main/java/com/pipai/dragontiles/spells/elementalist/ReturnToSender.kt
+++ core/src/main/java/com/pipai/dragontiles/spells/elementalist/ReturnToSender.kt
@@ -3,6 +3,7 @@ package com.pipai.dragontiles.spells.elementalist
 import com.pipai.dragontiles.combat.CombatApi
 import com.pipai.dragontiles.combat.CombatSubscribe
 import com.pipai.dragontiles.combat.SwapEvent
+import com.pipai.dragontiles.data.Keywords
 import com.pipai.dragontiles.spells.*
 
 class ReturnToSender : StandardSpell() {
@@ -12,15 +13,19 @@ class ReturnToSender : StandardSpell() {
     override val targetType: TargetType = TargetType.NONE
     override val rarity: Rarity = Rarity.UNCOMMON
     override val aspects: MutableList<SpellAspect> = mutableListOf(
-        FluxLossAspect(15, false),
+        FluxLossAspect(10, false),
     )
+
+    override fun additionalKeywords(): List<String> {
+        return listOf(Keywords.SWAP)
+    }
 
     override suspend fun onCast(params: CastParams, api: CombatApi) {
     }
 
     @CombatSubscribe
     suspend fun onSwap(ev: SwapEvent, api: CombatApi) {
-        if (ev.activeSpells.contains(this)) {
+        if (ev.activeSpells.contains(this) || ev.sideboardSpells.contains(this)) {
             api.heroLoseFlux(baseFluxLoss())
         }
     }
