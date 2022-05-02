9ff1ddec1c5efdb4525d11d05061ebeb5ef69646
Cheng-Yu Pai <mail4charliepai@gmail.com>
2022-05-02 02:27:50 -0700

    fix descs

--- core/src/main/java/com/pipai/dragontiles/spells/elementalist/Discharge.kt
+++ core/src/main/java/com/pipai/dragontiles/spells/elementalist/Discharge.kt
@@ -2,6 +2,7 @@ package com.pipai.dragontiles.spells.elementalist
 
 import com.pipai.dragontiles.combat.CombatApi
 import com.pipai.dragontiles.data.Element
+import com.pipai.dragontiles.data.TileInstance
 import com.pipai.dragontiles.spells.*
 
 class Discharge : StandardSpell() {
@@ -12,10 +13,14 @@ class Discharge : StandardSpell() {
     override val rarity: Rarity = Rarity.UNCOMMON
     override val aspects: MutableList<SpellAspect> = mutableListOf()
 
+    override fun dynamicBaseDamage(components: List<TileInstance>, api: CombatApi): Int {
+        return api.runData.hero.flux / 2
+    }
+
     override suspend fun onCast(params: CastParams, api: CombatApi) {
         val target = api.getEnemy(params.targets.first())
         val amount = api.runData.hero.flux / 2
         api.heroLoseFlux(amount)
-        api.attack(target, Element.LIGHTNING, baseDamage(), flags())
+        api.attack(target, Element.LIGHTNING, amount, flags())
     }
 }
